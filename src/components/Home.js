359e5dc12d7ba0969e917287ef4a55c4d3105c1e
Christian R. Lara <christianramirezlara1@gmail.com>
2023-06-08 08:58:45 -0600

    Init project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
 export const Home = (onNavigate) => {
   const HomeDiv = document.createElement('div');
   const buttonLogin = document.createElement('button');
-
   buttonLogin.textContent = 'Inicia sesión';
+  //buttonLogin.setAttribute('type', 'submit');
 
-  buttonLogin.addEventListener('click', () => onNavigate('/login'));
+  buttonLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+    onNavigate('/login')
+  });
+
+  HomeDiv.innerHTML += `
+    <input placeholder="Correo electrónico" type="email">
+    <input placeholder="Contraseña" type="password">
+    <img src="./../btn-google.png" class="button-google" alt="button">
+  `;
 
   HomeDiv.appendChild(buttonLogin);
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,36 @@
 export const Login = (onNavigate) => {
   const HomeDiv = document.createElement('div');
-  HomeDiv.textContent = 'Bienvenida al Login';
+  HomeDiv.textContent = 'Bienvenida SocialTaco';
+  HomeDiv.className = 'home-div';
   const buttonHome = document.createElement('button');
 
-  buttonHome.textContent = 'Regresar al Home';
+  buttonHome.classList = 'home-div__button';
+  buttonHome.textContent = 'Regresar al Login';
 
   buttonHome.addEventListener('click', () => onNavigate('/'));
+
+  
+
+  HomeDiv.innerHTML += `
+  <div class="new-post__container">
+    <textarea class="new-post__container__textarea"></textarea>
+    <button class="new-post__container__button">Publicar</button>
+  </div>  
+  <section class="posts">
+    <div class="posts__post">
+      <p>Estos tacos me hicieron daño.</p>
+      <h6>Christian R. Lara</h6>
+    </div>
+    <div class="posts__post">
+      <p>Estos tacos me hicieron daño.</p>
+      <h6>Christian R. Lara</h6>
+    </div>
+    <div class="posts__post">
+      <p>Estos tacos me hicieron daño.</p>
+      <h6>Christian R. Lara</h6>
+    </div>
+  </section>
+  `;
 
   HomeDiv.appendChild(buttonHome);
 
